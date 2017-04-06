e90b6a330547c6fdd21c27ee53d8cda7b769b217
Mike Taylor <20064376@mail.wit.ie>
2017-04-06 16:38:42 +0100

    Initial Commit

---
+++ rainorshine WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  rainorshine WatchKit Extension
+//
+//  Created by Mike Taylor on 06/04/2017.
+//  Copyright © 2017 Mike Taylor. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ rainorshineTests/rainorshineTests.swift
@@ -0,0 +1,36 @@
+//
+//  rainorshineTests.swift
+//  rainorshineTests
+//
+//  Created by Mike Taylor on 06/04/2017.
+//  Copyright © 2017 Mike Taylor. All rights reserved.
+//
+
+import XCTest
+@testable import rainorshine
+
+class rainorshineTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
