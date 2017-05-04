039257deb7dae10a4434c18c0886bd0db4dfefd7
Mike Taylor <20064376@mail.wit.ie>
2017-05-04 15:07:10 +0100

    added in custom navigation bar

--- rainorshine/ViewController.swift
+++ rainorshine/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationController?.navigationBar.barTintColor = UIColor(red:0.18, green:0.69, blue:0.82, alpha:1.0)
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
         // Do any additional setup after loading the view, typically from a nib.
     }
 
